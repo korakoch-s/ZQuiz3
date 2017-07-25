963d3f4a0ea8c099c9b34ba604fe2b4b9376219a
Korakoch Singkam <korakoch.s@gmail.com>
2017-07-26 03:36:54 +0700

    Prepare services unit tests.

---
+++ ZQuiz.BusinessServices.Tests/ZQuizServicesTests.cs
@@ -0,0 +1,62 @@
+﻿using NUnit.Framework;
+using ZQuiz.BusinessServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZQuiz.BusinessServices.Tests
+{
+    [TestFixture()]
+    public class ZQuizServicesTests
+    {
+        [Test()]
+        public void RegisterTest()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Test()]
+        public void ZQuizServicesTest()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Test()]
+        public void CalculateRankingTest()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Test()]
+        public void GetAllQuestionsTest()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Test()]
+        public void LoadTesterByNameTest()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Test()]
+        public void RegisterTest1()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Test()]
+        public void SaveTestTest()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Test()]
+        public void SubmitTestTest()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
