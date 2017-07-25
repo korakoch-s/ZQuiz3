eaea2d2488841b1c4fa71ef2e2dbf69e90c2086b
Korakoch Singkam <korakoch.s@gmail.com>
2017-07-26 03:06:57 +0700

    Done DataModel

--- ZQuiz.DataModel/DependencyResolver.cs
+++ ZQuiz.DataModel/DependencyResolver.cs
@@ -1,12 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.Composition;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ZQuiz.DataModel.UnitOfWork;
+using ZQuiz.Resolver;
 
 namespace ZQuiz.DataModel
 {
-    public class DependencyResolver 
+    [Export(typeof(IComponent))]
+    public class DependencyResolver : IComponent
     {
+        public void SetUp(IRegisterComponent registerComponent)
+        {
+            registerComponent.RegisterType<IUnitOfWork, UnitOfWork.UnitOfWork>();
+        }
     }
 }
