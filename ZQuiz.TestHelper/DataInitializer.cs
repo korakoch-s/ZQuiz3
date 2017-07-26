e0991780bf98f3c7b30476946f4e4a41e852683f
Korakoch Singkam <korakoch.s@gmail.com>
2017-07-26 09:16:01 +0700

    Draft testing helper class and dummy data

---
+++ ZQuiz.TestHelper/DataInitializer.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZQuiz.DataModel;
+
+namespace ZQuiz.TestHelper
+{
+    /// <summary>
+    /// Data initializer for unit tests
+    /// </summary>
+    public class DataInitializer
+    {
+        /// <summary>
+        /// Dummy questions data
+        /// </summary>
+        /// <returns></returns>
+        public static List<Question> GetAllQuestions()
+        {
+            var questions = new List<Question>();
+
+            for (int i = 1; i <= 5; i++)
+            {
+                var question = new Question()
+                {
+                    Title = "Question number " + i,
+                    TotalScore = 10,
+                    Choices = new List<Choice>()
+                };
+                for (int j = 1; j <= 5; j++)
+                {
+                    var choice = new Choice()
+                    {
+                        Title = "Choice " + j + " of " + question.Title,
+                        Score = j * 2
+                    };
+                    question.Choices.Add(choice);
+                }
+                questions.Add(question);
+            }
+
+            return questions;
+        }
+
+        /// <summary>
+        /// Dummy testers data
+        /// </summary>
+        /// <returns></returns>
+        public static List<Tester> GetAllTesters()
+        {
+            var testers = new List<Tester>();
+            for (int i=1; i<=5; i++)
+            {
+                var tester = new Tester()
+                {
+                    Name = "Tester " + i,
+                    IsCompleted = false
+                };
+                testers.Add(tester);
+            }
+
+            return testers;
+        }
+
+    }
+}
---
+++ ZQuiz.TestHelper/TesterComparer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZQuiz.DataModel;
+
+namespace ZQuiz.TestHelper
+{
+    public class TesterComparer : IComparer, IComparer<Tester>
+    {
+        public int Compare(object expected, object actual)
+        {
+            var lhs = expected as Tester;
+            var rhs = actual as Tester;
+            if (lhs == null || rhs == null) throw new InvalidOperationException();
+            return Compare(lhs, rhs);
+        }
+
+        public int Compare(Tester expected, Tester actual)
+        {
+
+            int temp = 0;
+
+            temp = expected.TesterId.CompareTo(actual.TesterId) != 0 ? temp : expected.Name.CompareTo(actual.Name);
+
+            return temp;
+        }
+    }
+}
